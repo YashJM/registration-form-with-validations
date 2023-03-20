53ad656ac8d1d9f3c36e28b0852d3828bb06f647
Yash Mistry <mistryyash100@gmail.com>
2023-03-20 12:49:51 -0400

    added spacing (#11)

--- src/views/Registration.tsx
+++ src/views/Registration.tsx
@@ -7,7 +7,7 @@ import RegistrationForm from '../components/RegistrationForm';
 const Registration: React.FC = () => {
   return (
     <Box width={'100%'}>
-      <Box maxWidth={'500px'} margin={'0 auto'}>
+      <Box maxWidth={'520px'} margin={'0 auto'} px={'20px'}>
         <Box
           component={'span'}
           sx={{ fontWeight: 'bold', fontSize: '20px', mb: '15px' }}
